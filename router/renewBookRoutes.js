605f5dc5930cf9dbdf460d8f39d40203afb2bf44
Saoni Mustafi <saonimustafi1994@gmail.com>
2023-04-09 00:31:36 -0700

    Modified the renewBookRoutes to function correctly and updated the JSON responses.

--- router/renewBookRoutes.js
+++ router/renewBookRoutes.js
@@ -5,52 +5,63 @@ const userModel = require('../models/users')
 const bookModel = require("../models/books")
 
 // PUT Operator - Renew requests for users with book_id
-renewBookRouter.put('/activities/renew/user_id/book_id', async(request, response) => {
+renewBookRouter.put('/activities/renew/:user_id/:book_id', async(request, response) => {
     const user_id = request.params.user_id
     const book_id = request.params.book_id
 
     try {
-        const userActivityDetail = await userActivitiesModel.find({user_id: user_id, "booksBorrowed.book_id": book_id, "booksBorrowed.actualReturnDate": null})
+        const userActivityDetail = await userActivitiesModel.aggregate([{$match: {"user_id": Number(user_id)}}, {$project:
+                {books: {$filter: {input: "$booksBorrowed", as: "book",
+                            cond: {$and: [{$eq: ["$$book.book_id", Number(book_id)]}, {$eq: ["$$book.actualReturnDate", null]}]}}
+                    }
+                }}
+        ])
 
         if(!userActivityDetail) {
-            response.status(404).send("No current activity exists for the user")
+            response.status(404).send({"message":"No current activity exists for the user"})
             return
         }
 
-        const booksBorrowDetail = await userActivityDetail.booksBorrowed.find(book => book.book_id == book_id)
+        const booksBorrowDetail = await userActivityDetail[0].books.find(book => book.book_id === Number(book_id))
 
         if(!booksBorrowDetail) {
-            response.send(404).status("Book not found in user's activity")
+            response.send(404).status({"message":"Book not found in user's activity"})
         }
 
-        const currentDate = Date.now()
+        const currentDate = new Date(Date.now())
         const returnDate = booksBorrowDetail.returnDate
 
-        if(currDate <= returnDate) {
+        if(currentDate <= returnDate) {
             // Change the returnDate to two months more than the currentDate
             const newReturnDate = new Date(returnDate)
-            newReturnDate.setMonth(currentDate.getMonth()+3)
-            const newReturnYear = currentDate.getFullYear()
-            const newReturnMonth = currentDate.getMonth() + 1
-            const newReturnDay = currentDate.getDate()
+
+            const newReturnYear = newReturnDate.getFullYear()
+            const newReturnMonth = newReturnDate.getMonth() + 2
+            const newReturnDay = newReturnDate.getDate()
 
             const newReturnDateString = `${newReturnYear}-${newReturnMonth}-${newReturnDay}`
             const newReturnDateForUser = new Date(newReturnDateString)
 
-            await userActivityDetail.updateOne({user_id: user_id, "booksBorrowed.book_id": book_id, "booksBorrowed.actualReturnDate": null},
+            await userActivitiesModel.updateOne({user_id: Number(user_id), "booksBorrowed.book_id": Number(book_id), "booksBorrowed.actualReturnDate": null},
                 {$set:
                         {"booksBorrowed.$.returnDate": newReturnDateForUser}
                 }
             )
-            response.status(200).send(userActivityDetail)
+            const newUserActivityDetail = await userActivitiesModel.aggregate([{$match: {"user_id": Number(user_id)}}, {$project:
+                    {books: {$filter: {input: "$booksBorrowed", as: "book",
+                                cond: {$eq: ["$$book.book_id", Number(book_id)]}}
+                        }
+                    }}
+            ])
+            response.status(200).send({"newReturnDate": newReturnDateForUser})
         }
         else {
-            response.status(400).send("Please return the book and pay the fine. Then you can raise a request to borrow the book")
+            response.status(400).send({"message":"Please return the book and pay the fine. Then you can raise a request to borrow the book"})
         }
     }
     catch(error) {
         console.error(error)
-        response.status(500).send("PUT Operation failed while renewing requests for users with book id")
+        response.status(500).send({"message":"PUT Operation failed while renewing requests for users with book id"})
     }
 })
 
