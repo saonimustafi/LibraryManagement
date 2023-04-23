a2f29fba850283d2dd30077cc55c31e9917e2049
saonimustafi <saonimustafi1994@gmail.com>
2023-04-23 02:59:37 -0700

    Add notification system

--- models/Notification.js
+++ models/Notification.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 const notificationSchema = new mongoose.Schema({
+    id: {
+      type: Number,
+      required: true,
+      unique: true
+    },
     message: String,
     timestamp: { type: Date, default: Date.now },
-    userId: String,
-    bookId: String,
-    read: Boolean
+    userId: Number,
+    read: { type: Boolean, default: false }
   });
 
 const Notification = mongoose.model('Notification', notificationSchema)
